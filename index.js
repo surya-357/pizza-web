b532bab53148d6c94ca2a8913412aa473888ee72
surya-357 <101453261+surya-357@users.noreply.github.com>
2022-08-05 13:36:13 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,33 @@
+function login()
+{
+    var Email = document.getElementById("email").value;
+    var password = document.getElementById("password").value;
+    var filter = /^([a-zA-Z0-9_\.\-])+\@(([a-zA-Z0-9\-])+\.)+([a-zA-Z0-9]{2,4})+$/;
+    if(Email =='')
+    {
+        alert("please enter user name.");
+    }
+    else if(password=='')
+    {
+        alert("enter the password");
+    }
+    else if(!filter.test(Email))
+    {
+        alert("Enter valid email id.");
+    }
+    else if(password.length < 10 || password.length > 11)
+    {
+        alert("Password min and max length is 10.");
+    }
+    else
+    {
+alert('Thank You for Login & You are Redirecting to Campuslife Website');
+//    window.location = "https://www.campuslife.co.in";
+        }
+}
+// //Reset Inputfield code.
+function Clear()
+{
+    document.getElementById("email").value=" ";
+    document.getElementById("password").value="";
+}	
